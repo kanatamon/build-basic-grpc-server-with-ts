c689d378f87f0d7be8d99d4149047396a4667223
kanatamon <ponde.moonlord@gmail.com>
2022-09-05 11:15:21 +0700

    feat: add client-side stream

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,35 @@ import {
 } from '@grpc/grpc-js';
 import { loadSync } from '@grpc/proto-loader';
 import path from 'path';
+import invariant from 'tiny-invariant';
+
+import type { ServerReadableStream } from '@grpc/grpc-js';
 import type { ProtoGrpcType } from './proto/chat';
 import type { ServerUnaryCall, sendUnaryData } from '@grpc/grpc-js';
 import type { ChatHandlers } from './proto/chat/Chat';
-import type { Empty__Output } from './proto/chat/Empty';
+import type { Empty, Empty__Output } from './proto/chat/Empty';
 import type { MessageList } from './proto/chat/MessageList';
+import type { Message, Message__Output } from './proto/chat/Message';
 
 const PROTO_PATH = path.resolve(__dirname, '../proto/chat.proto');
+
+const messages = [{ id: 0, username: 'Nunan', text: 'Hello World 🌈' }];
+
+function getMessageList(): MessageList {
+  return { messages };
+}
+
+function addMessage(incomingMessage: Message): void {
+  invariant(incomingMessage.username, `Must be non-empty string`);
+  invariant(incomingMessage.text, `Must be non-empty string`);
+
+  const newMessage: Message__Output = {
+    id: messages.length,
+    username: incomingMessage.username,
+    text: incomingMessage.text,
+  };
+  messages.push(newMessage);
+}
 
 const chatHandlers: ChatHandlers = {
   GetMessages(
@@ -19,9 +41,17 @@ const chatHandlers: ChatHandlers = {
     callback: sendUnaryData<MessageList>
   ): void {
     console.log(`${new Date().toISOString()} - GetMessages`);
-    callback(null, {
-      messages: [{ id: 1, username: 'Nunan', text: 'Hello World 🌈' }],
+    callback(null, getMessageList());
+  },
+  AddMessages: function (
+    call: ServerReadableStream<Message__Output, Empty>,
+    callback: sendUnaryData<Empty>
+  ): void {
+    console.log(`${new Date().toISOString()} - AddMessage`);
+    call.on('data', (incomingMessage: Message) => {
+      addMessage(incomingMessage);
     });
+    call.on('end', () => callback(null, {}));
   },
 };
 
--- src/proto/chat/Chat.ts
+++ src/proto/chat/Chat.ts
@@ -7,11 +7,48 @@ import type {
   Empty__Output as _chat_Empty__Output,
 } from '../chat/Empty';
 import type {
+  Message as _chat_Message,
+  Message__Output as _chat_Message__Output,
+} from '../chat/Message';
+import type {
   MessageList as _chat_MessageList,
   MessageList__Output as _chat_MessageList__Output,
 } from '../chat/MessageList';
 
 export interface ChatClient extends grpc.Client {
+  AddMessages(
+    metadata: grpc.Metadata,
+    options: grpc.CallOptions,
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  AddMessages(
+    metadata: grpc.Metadata,
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  AddMessages(
+    options: grpc.CallOptions,
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  AddMessages(
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  addMessages(
+    metadata: grpc.Metadata,
+    options: grpc.CallOptions,
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  addMessages(
+    metadata: grpc.Metadata,
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  addMessages(
+    options: grpc.CallOptions,
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+  addMessages(
+    callback: grpc.requestCallback<_chat_Empty__Output>
+  ): grpc.ClientWritableStream<_chat_Message>;
+
   GetMessages(
     argument: _chat_Empty,
     metadata: grpc.Metadata,
@@ -55,10 +92,21 @@ export interface ChatClient extends grpc.Client {
 }
 
 export interface ChatHandlers extends grpc.UntypedServiceImplementation {
+  AddMessages: grpc.handleClientStreamingCall<
+    _chat_Message__Output,
+    _chat_Empty
+  >;
+
   GetMessages: grpc.handleUnaryCall<_chat_Empty__Output, _chat_MessageList>;
 }
 
 export interface ChatDefinition extends grpc.ServiceDefinition {
+  AddMessages: MethodDefinition<
+    _chat_Message,
+    _chat_Empty,
+    _chat_Message__Output,
+    _chat_Empty__Output
+  >;
   GetMessages: MethodDefinition<
     _chat_Empty,
     _chat_MessageList,
